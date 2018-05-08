2ef65054b10bc3d872f1ba39cfb1371a47bb2493
Pyry Pajunpää <pyry.pajunpaa@houston-inc.com>
2018-05-08 22:36:11 +0300

    first filter working

--- filter.js
+++ filter.js
@@ -1,11 +1,13 @@
 const condition = row => {
   return (
-    true
+    row.time_on_task !== '0'
+    // true
   );
   // Implement filtering conditions here
 };
 
 exports.filterSubmissions = dataObj => {
   const filteredSubmissions = dataObj.submissions.filter(condition);
+  console.log(filteredSubmissions.length)
   return filteredSubmissions;
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const parser = require("./csvparser");
 const filter = require("./filter");
 const controller = require("./controller");
-const plotter = require('./plotter');
+const plotter = require("./plotter");
 
 const assignmentsPath = "./data/assignments.csv";
 const submissions1Path = "./data/submissions1.csv";
@@ -19,9 +19,15 @@ async function execute() {
   console.log("starting program execution");
   await parseFiles();
   console.log("parsing completed");
-  const dataObj = controller.createDataObj(submissions1Json, submissions2Json, assignmentsJson, backgroundVariablesJson);
+  const dataObj = controller.createDataObj(
+    submissions1Json,
+    submissions2Json,
+    assignmentsJson,
+    backgroundVariablesJson
+  );
   // console.log(console.log(submissions1Json));
-  filterSubmissions(dataObj);
+  const filteredObj = filterSubmissions(dataObj);
+  console.log(filteredObj.submissions.length);
   // runController();
   // plotter.plot(dataObj);
 }
@@ -38,8 +44,8 @@ async function parseFiles() {
   // ]);
 }
 
-const filterSubmissions = async (dataObj) => {
-  return await Promise.all([filter.filterSubmissions(dataObj)]);
+const filterSubmissions = dataObj => {
+  return { ...dataObj, submissions: filter.filterSubmissions(dataObj) };
 };
 
 const runController = () => {
