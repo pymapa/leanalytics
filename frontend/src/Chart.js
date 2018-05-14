f8ba8c15baebfc5de98516445594ca9fdd18e0dc
Pyry Pajunpää <pyry.pajunpaa@houston-inc.com>
2018-05-14 17:56:20 +0300

    chart changes. relative value to score

--- frontend/src/Chart.js
+++ frontend/src/Chart.js
@@ -4,28 +4,48 @@ import { Scatter } from "react-chartjs-2";
 class Chart extends Component {
   getData1() {
     return this.props.data.selfEval1.map(submission => {
-      return { x: submission.assign_id, y: submission.score };
+      return {
+        x: submission.assign_id,
+        y: this.calculateRelativeScore(submission)
+      };
     });
   }
   getData2() {
     return this.props.data.selfEval2.map(submission => {
-      return { x: submission.assign_id, y: submission.score };
+      return {
+        x: submission.assign_id,
+        y: this.calculateRelativeScore(submission)
+      };
     });
   }
   getData3() {
     return this.props.data.selfEval3.map(submission => {
-      return { x: submission.assign_id, y: submission.score };
+      return {
+        x: submission.assign_id,
+        y: this.calculateRelativeScore(submission)
+      };
     });
   }
   getData4() {
     return this.props.data.selfEval4.map(submission => {
-      return { x: submission.assign_id, y: submission.score };
+      console.log(submission.time_on_task)
+      return {
+        x: submission.assign_id,
+        y: this.calculateRelativeScore(submission)
+      };
     });
   }
   getData5() {
     return this.props.data.selfEval5.map(submission => {
-      return { x: submission.assign_id, y: submission.score };
+      return {
+        x: submission.assign_id,
+        y: this.calculateRelativeScore(submission)
+      };
     });
+  }
+
+  calculateRelativeScore(submission) {
+    return submission.score / submission.time_on_task;
   }
   render() {
     const data1 = this.getData1();
@@ -33,6 +53,19 @@ class Chart extends Component {
     const data3 = this.getData3();
     const data4 = this.getData4();
     const data5 = this.getData5();
+    const options = {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              min: 0,
+              suggestedMax: 120,
+              stepSize: 5
+            }
+          }
+        ]
+      }
+    };
     const chartData = {
       labels: ["Scatter"],
       datasets: [
@@ -46,7 +79,7 @@ class Chart extends Component {
           pointHoverBackgroundColor: "rgba(65, 173, 53,1)",
           pointHoverBorderColor: "rgba(65, 173, 530,1)",
           pointHoverBorderWidth: 2,
-          pointRadius: 1,
+          pointRadius: 2,
           pointHitRadius: 10,
           data: data1
         },
@@ -60,7 +93,7 @@ class Chart extends Component {
           pointHoverBackgroundColor: "rgba(37,19,19,1)",
           pointHoverBorderColor: "rgba(37,19,19,1)",
           pointHoverBorderWidth: 2,
-          pointRadius: 1,
+          pointRadius: 2,
           pointHitRadius: 10,
           data: data2
         },
@@ -74,7 +107,7 @@ class Chart extends Component {
           pointHoverBackgroundColor: "rgba(65, 244, 235,1)",
           pointHoverBorderColor: "rgba(65, 244, 235,1)",
           pointHoverBorderWidth: 2,
-          pointRadius: 1,
+          pointRadius: 2,
           pointHitRadius: 10,
           data: data3
         },
@@ -88,7 +121,7 @@ class Chart extends Component {
           pointHoverBackgroundColor: "rgba(244, 72, 66,1)",
           pointHoverBorderColor: "rgba(244, 72, 66,1)",
           pointHoverBorderWidth: 2,
-          pointRadius: 1,
+          pointRadius: 2,
           pointHitRadius: 10,
           data: data4
         },
@@ -102,7 +135,7 @@ class Chart extends Component {
           pointHoverBackgroundColor: "rgba(168, 107, 255,1)",
           pointHoverBorderColor: "rgba(168, 107, 255,1)",
           pointHoverBorderWidth: 2,
-          pointRadius: 1,
+          pointRadius: 2,
           pointHitRadius: 10,
           data: data5
         }
@@ -110,7 +143,7 @@ class Chart extends Component {
     };
     return (
       <div>
-        <Scatter data={chartData} />
+        <Scatter data={chartData} options={options} />
       </div>
     );
   }
