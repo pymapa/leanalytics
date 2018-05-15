553bb9d0e4bf039abe0793947ad3a105c8f9adef
niemisami <sami.nieminen@madbooster.com>
2018-05-15 17:02:44 +0300

    reorder chart functions

--- frontend/src/Chart.js
+++ frontend/src/Chart.js
@@ -2,8 +2,32 @@ import React, { Component } from "react";
 import { Scatter, defaults } from "react-chartjs-2";
 defaults.global.animation = false;
 
-let a = 0;
-let b = 0;
+const calculateMedian = (values) => {
+  values.sort((a, b) => a - b);
+
+  if(values.length === 0) return 0
+
+  var half = Math.floor(values.length / 2);
+
+  if(values.length % 2)
+    return values[half];
+  else
+    return (values[half - 1] + values[half]) / 2.0;
+}
+
+const calculateMode = arr =>
+  arr.reduce((current, item) => {
+    if(item === 0) {
+      return current
+    }
+    let val = current.numMapping[item] = (current.numMapping[item] || 0) + 1;
+    if(val > current.greatestFreq) {
+      current.greatestFreq = val;
+      current.mode = item;
+    }
+    return current;
+  }, { mode: null, greatestFreq: -Infinity, numMapping: {} }, arr).mode;
+
 
 class Chart extends Component {
   getData1() {
@@ -47,85 +71,49 @@ class Chart extends Component {
     });
   }
 
-  median(values){
-    values.sort(function(a,b){
-    return a-b;
-  });
-
-  if(values.length ===0) return 0
-
-  var half = Math.floor(values.length / 2);
-
-  if (values.length % 2)
-    return values[half];
-  else
-    return (values[half - 1] + values[half]) / 2.0;
-}
-
   calculateRelativeScore(submission) {
     const score = submission.score / (submission.max_points * submission.time_on_task);
     return score;
   }
-  reduceAvg = (sum, d) => {
-    if(isNaN(d.y) || d.y == 'Infinity') {
-      a++;
-      return sum
+
+  calculateResults = data => {
+    let invalid = 0
+    const reduceAvg = (sum, d) => {
+      if(isNaN(d.y) || !isFinite(d.y)) {
+        invalid++;
+        return sum
+      }
+      return sum + d.y
     }
-    b++;
-    return sum + d.y
+    const arr = data.map(d => d.y)
+    const sum = data.reduce(reduceAvg, 0)
+    const avg = sum / data.length
+    const median = calculateMedian(arr)
+    const mode = calculateMode(arr)
+    return {
+      length: data.length,
+      sum,
+      invalid,
+      avg,
+      median,
+      mode
+    }
   }
   render() {
     const data1 = this.getData1();
-    const avg1 = data1.reduce(this.reduceAvg, 0) / b
-    a = 0;
-    b = 0;
-    const median1 = this.median(data1.map(d => d.y))
-    console.log('Data 1', avg1, data1.length, median1)
+    console.log('Data 1', this.calculateResults(data1))
 
     const data2 = this.getData2();
-    const avg2 = data2.reduce(this.reduceAvg, 0) / b
-    a = 0;
-    b = 0;
-    const median2 = this.median(data2.map(d => d.y))
-    console.log('Data 2', avg2, data2.length, median2)
+    console.log('Data 2', this.calculateResults(data2))
 
     const data3 = this.getData3();
-    const avg3 = data3.reduce(this.reduceAvg, 0) / b
-    a = 0;
-    b = 0;
-    const median3 = this.median(data3.map(d => d.y))
-    console.log('Data 3', avg3, data3.length, median3)
+    console.log('Data 3', this.calculateResults(data3))
 
     const data4 = this.getData4();
-    const avg4 = data4.reduce(this.reduceAvg, 0) / b
-    a = 0;
-    b = 0;
-    const median4 = this.median(data4.map(d => d.y))
-    console.log('Data 4', avg4, data4.length, median4)
+    console.log('Data 4', this.calculateResults(data4))
 
     const data5 = this.getData5();
-    const avg5 = data5.reduce(this.reduceAvg, 0) / b
-    a = 0;
-    b = 0;
-    const median5 = this.median(data5.map(d => d.y))
-    console.log('Data 5', avg5, data5.length, median5)
-
-    const medians = [{
-      x: 30000,
-      y: avg1
-    }, {
-      x: 30000,
-      y: avg2
-    }, {
-      x: 30000,
-      y: avg3
-    }, {
-      x: 30000,
-      y: avg4
-    }, {
-      x: 30000,
-      y: avg5
-    }]
+    console.log('Data 5', this.calculateResults(data5))
 
     const options = {
       scales: {
@@ -212,20 +200,6 @@ class Chart extends Component {
           pointRadius: 2,
           pointHitRadius: 10,
           data: data5
-        },
-        {
-          label: "Medians",
-          backgroundColor: "rgba(168, 107, 2551)",
-          pointBorderColor: "rgba(168, 107, 255,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 6,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(168, 107, 255,1)",
-          pointHoverBorderColor: "rgba(168, 107, 255,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 6,
-          pointHitRadius: 10,
-          data: medians
         }
       ]
     };
